95a6e0fea4ac7a12de8cb2705244d506066f62fc
Artem Popov <artm.ppv28@gmail.com>
2023-08-07 16:47:58 +0300

    Добавлены тестовые примеры для std::auto_ptr и std::unique_ptr

--- smart_pointers/main.cpp
+++ smart_pointers/main.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
-
+#include <memory>
 
 
 
 int main()
 {
-	std::cout << "hello, Artem! \n";
-	std::cout << "hello, Hasky! \n";
-	std::auto_ptr<int> a;
+	/*auto_ptr test*/
+  std::auto_ptr<int> a_a(new int(3));
+  std::cout << *a_a << "\n";
+
+  std::auto_ptr<int> a_b = a_a;
+  std::cout << *a_b << "\n";
+ //  std::cout << *a_a << "\n";  //здесь ошибка, так как а отдал права пользования, теперь он null_pointer
+
+ // std::auto_ptr<int> a_c =  std::auto_ptr<int>(a_a);  //здесь тa же ошибка, 
+ // std::cout << *a_a << "\n";                          
+
+  std::auto_ptr<int> a_d = std::auto_ptr<int>(new int(2)); //здесь временный объект, ошибки не будет, auto_ptr ничего не знает про rvalue
+
+
+     /*unique_ptr test*/
+  std::unique_ptr<int> u_a(new int(5));
+  std::cout << *u_a << "\n";
+
+ // std::unique_ptr<int> u_b = u_aa; //ошибка, так как unique_ptr запрещает передачу прав пользования памятью
+
+  std::unique_ptr<int> u_c = std::unique_ptr<int>(new int(2));
+  std::cout << *u_c << "\n";
+   
+
 }
