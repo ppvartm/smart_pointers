27dd6db4916b4e68f1000871ca936bf99adb8339
ppvartm <artm.ppv28@gmail.com>
2023-10-01 22:38:54 +0300

    Полностью изменить unique_ptr и shared_ptr - результат прочтения книги Мейерса и новое осознание smart pointers :)

--- smart_pointers/shared_ptr.h
+++ smart_pointers/shared_ptr.h
@@ -1,67 +1,230 @@
 #pragma once
-
-template<typename U>
-struct control_block
-{
-	size_t counter_;
-	U object;
-
-	template<typename ... Args>
-	control_block(size_t count, Args&& ... args) :counter_(count), object(std::forward<U>(args)...) {};
-};
+#include<functional>
 
 template<typename T>
+class weak_ptr;
+
+template <typename T>
 class shared_ptr
 {
 private:
 
-	T* ptr = nullptr;
-	size_t* counter = nullptr;
+	friend class weak_ptr<T>;
 
-	template <typename U, typename ... Args>
-	friend shared_ptr<U> make_shared(Args&& ... args);
+	//T* ptr;
+	//size_t* counter;
+	template<typename T, typename ... D>
+	friend shared_ptr<T> make_shared(D&&... a);
 
-	shared_ptr(control_block<T>* cb) :ptr(reinterpret_cast<T*>(& cb->object)), counter(reinterpret_cast<size_t*>(&cb->counter_)) {};
+	struct object
+	{
+		T obj;
+		object(const T& ptr_) :obj(ptr_) {};
+		object(T&& ptr_) :obj(ptr_) {};
+	};
+	struct control_block_for_make_shared : public object
+	{
+		size_t shared_counter;
+		size_t weak_counter;
+		control_block_for_make_shared(const T& ob, size_t sc, size_t wc) :object(ob), shared_counter(sc), weak_counter(wc) {};
+	};
+	struct control_block_for_constructor
+	{
+		size_t shared_counter;
+		size_t weak_counter;
+		std::function<void(T*)> deleter = [](T* ptr) {delete ptr; };
+		control_block_for_constructor(size_t sc, size_t wc) :shared_counter(sc), weak_counter(wc) {};
+		control_block_for_constructor(size_t sc, size_t wc, const std::function<void(T*)>& del) :shared_counter(sc), weak_counter(wc), deleter(del) {};
+	};
 
+	object* pointer;
+	control_block_for_constructor* defoalt_control_block = nullptr;
 
+	shared_ptr(control_block_for_make_shared* cb) : pointer(cb) {};
+	shared_ptr(object* pointer_, control_block_for_constructor* control_block_) :pointer(pointer_), defoalt_control_block(control_block_)
+	{
+		if (defoalt_control_block == nullptr)
+			++(static_cast<control_block_for_make_shared*>(pointer)->shared_counter);
+		else ++(defoalt_control_block->shared_counter);
+	}
 public:
-	shared_ptr(T* ptr_) :ptr(ptr_), counter(new size_t(1)) {}
+	shared_ptr() :pointer(nullptr), defoalt_control_block(nullptr) {}
+	shared_ptr(T* ptr_) :pointer(reinterpret_cast<object*>(ptr_)), defoalt_control_block(new control_block_for_constructor(1, 0)) {}
+	template<typename Del>
+	shared_ptr(T* ptr_, const Del& deleter_) : pointer(reinterpret_cast<object*>(ptr_)), defoalt_control_block(new control_block_for_constructor(1, 0, deleter_)) {}
 
-	shared_ptr(const shared_ptr<T>& ptr_):ptr(ptr_.ptr),counter(ptr_.counter)
-	{
-		++(*counter);
+
+	shared_ptr(const shared_ptr<T>& s_p) {
+		if (s_p.defoalt_control_block != nullptr)
+		{
+			pointer = s_p.pointer;
+			defoalt_control_block = s_p.defoalt_control_block;
+			++(defoalt_control_block->shared_counter);
+		}
+		else {
+			pointer = s_p.pointer;
+			++(static_cast<control_block_for_make_shared*>(pointer)->shared_counter);
+		}
 	}
-
-	T& operator *() const
+	shared_ptr(shared_ptr<T>&& s_p) noexcept
 	{
-		return *ptr;
+		if (s_p.defoalt_control_block != nullptr)
+		{
+			pointer = s_p.pointer;
+			defoalt_control_block = s_p.defoalt_control_block;
+			s_p.pointer = nullptr;
+			s_p.defoalt_control_block = nullptr;
+		}
+		else {
+			pointer = s_p.pointer;
+			s_p.pointer = nullptr;
+		}
 	}
-
 	~shared_ptr()
 	{
-		if (*counter > 1)
-			--(*counter);
+		if (defoalt_control_block == nullptr)
+		{
+			if (--(static_cast<control_block_for_make_shared*>(pointer)->shared_counter) == 0)
+			{
+				pointer->obj.~T();
+				if (static_cast<control_block_for_make_shared*>(pointer)->weak_counter == 0)
+					delete(pointer);
+			}
+		}
 		else
 		{
-			delete ptr;
-			delete counter;
+			if (--(defoalt_control_block->shared_counter) == 0)
+			{
+				defoalt_control_block->deleter(reinterpret_cast<T*>(pointer));
+				if (defoalt_control_block->weak_counter == 0)
+					delete(defoalt_control_block);
+			}
+		}
+	}
+	shared_ptr<T>& operator=(const shared_ptr<T>& s_p)
+	{
+		if (this == &s_p) return *this;
+		this->~shared_ptr();
+
+
+		if (s_p.defoalt_control_block != nullptr)
+		{
+			pointer = s_p.pointer;
+			defoalt_control_block = s_p.defoalt_control_block;
+			++(defoalt_control_block->shared_counter);
+		}
+		else {
+			pointer = s_p.pointer;
+			++(static_cast<control_block_for_make_shared*>(pointer)->shared_counter);
+		}
+		return *this;
+	}
+
+	shared_ptr<T>& operator=(shared_ptr<T>&& s_p) noexcept
+	{
+		this->~shared_ptr();
+
+		if (s_p.defoalt_control_block != nullptr)
+		{
+			pointer = s_p.pointer;
+			defoalt_control_block = s_p.defoalt_control_block;
+			s_p.pointer = nullptr;
+			s_p.defoalt_control_block = nullptr;
+		}
+		else {
+			pointer = s_p.pointer;
+			s_p.pointer = nullptr;
+		}
+		return *this;
+	}
+
+
+
+};
+
+template<typename T, typename ... D>
+shared_ptr<T> make_shared(D&&... args)
+{
+	auto p_cb = new  typename shared_ptr<T>::control_block_for_make_shared(T(std::forward<D>(args)...), 1, 0);
+	return shared_ptr<T>(p_cb);
+}
+
+template <typename T>
+class weak_ptr
+{
+private:
+	typename shared_ptr<T>::object* pointer;
+	typename shared_ptr<T>::control_block_for_constructor* cb_for_con;
+	//typename shared_ptr<T>::control_block_for_make_shared* cb_for_mk_sh;
+public:
+	weak_ptr(const shared_ptr<T>& sh_ptr)
+	{
+		if (sh_ptr.defoalt_control_block == nullptr)
+		{
+
+			cb_for_con = nullptr;
+			pointer = sh_ptr.pointer;
+			++(static_cast<typename shared_ptr<T>::control_block_for_make_shared*>(pointer)->weak_counter);
+		}
+		else
+		{
+			pointer = sh_ptr.pointer;
+			cb_for_con = sh_ptr.defoalt_control_block;
+			++(cb_for_con->weak_counter);
+		}
+	}
+
+	weak_ptr(const weak_ptr<T>& wk_ptr) :pointer(wk_ptr.pointer), cb_for_con(wk_ptr.cb_for_con)
+	{
+		if (cb_for_con == nullptr)
+			++(static_cast<typename shared_ptr<T>::control_block_for_make_shared*>(pointer)->weak_counter);
+		else ++(cb_for_con->weak_counter);
+	}
+
+	weak_ptr(weak_ptr<T>&& wk_ptr)noexcept :pointer(wk_ptr.pointer), cb_for_con(wk_ptr.cb_for_con)
+	{
+		wk_ptr.cb_for_con = nullptr;
+		wk_ptr.pointer = nullptr;
+	}
+
+	bool expired() const
+	{
+		if (cb_for_con == nullptr)
+			return !(static_cast<typename shared_ptr<T>::control_block_for_make_shared*>(pointer)->shared_counter);
+		return !(cb_for_con->shared_counter);
+	}
+	shared_ptr<T> lock()
+	{
+		if (cb_for_con == nullptr)
+		{
+			if (static_cast<typename shared_ptr<T>::control_block_for_make_shared*>(pointer)->shared_counter)
+				return shared_ptr<T>(pointer, cb_for_con);
+			return shared_ptr<T>();
+		}
+		else
+		{
+			if (cb_for_con->shared_counter)
+				return shared_ptr<T>(pointer, cb_for_con);
+			return shared_ptr<T>();
 		}
 	}
 
 
-	size_t use_count() const
+	~weak_ptr()
 	{
-		return *counter;
+		if (cb_for_con == nullptr)
+		{
+			--(static_cast<typename shared_ptr<T>::control_block_for_make_shared*>(pointer)->weak_counter);
+			if ((static_cast<typename shared_ptr<T>::control_block_for_make_shared*>(pointer)->weak_counter == 0) &&
+				(static_cast<typename shared_ptr<T>::control_block_for_make_shared*>(pointer)->shared_counter == 0))
+				delete pointer;
+		}
+		else
+		{
+			--(cb_for_con->weak_counter);
+			if ((cb_for_con->weak_counter == 0) && (cb_for_con->shared_counter == 0))
+				delete cb_for_con;
+		}
 	}
 
 };
-
-
-template<typename T, typename ... Args>
-shared_ptr<T> make_shared(Args&& ... args)
-{
-
-	
-	auto ptr_ = new control_block<T>(1, std::forward<T>(args)...);
-	return shared_ptr<T>(ptr_);
-}
--- smart_pointers/unique_ptr.h
+++ smart_pointers/unique_ptr.h
@@ -1,33 +1,100 @@
 #pragma once
 
+template <typename T>
+class unique_ptr_deleter
+{
+public:
+	void operator()(T* obj)
+	{
+		delete obj;
+	}
+};
 
-template<typename T>
+template <typename T>
+class unique_ptr_deleter<T[]>
+{
+public:
+	void operator()(T* obj)
+	{
+		delete[] obj;
+	}
+};
+
+
+template<typename T, typename D = unique_ptr_deleter<T>>
 class unique_ptr
 {
 private:
 	T* ptr;
+	D deleter;
 public:
-	unique_ptr(T* ptr_) :ptr(ptr_) {}
-	unique_ptr(const unique_ptr<T>& ptr_) = delete;
-	unique_ptr<T>& operator=(const unique_ptr<T>& ptr_) = delete;
+	unique_ptr(T* ptr_ = nullptr) noexcept :ptr(ptr_) {};
+	unique_ptr(T* ptr_, const D& deleter_)noexcept :ptr(ptr_), deleter(deleter_) {};
+	unique_ptr(const unique_ptr<T>& obj) = delete;
+	unique_ptr& operator=(const unique_ptr<T>& obj) = delete;
 
-	unique_ptr(unique_ptr<T>&& ptr_) :ptr(ptr_) { ptr_ = nullptr; }
-	unique_ptr<T>& operator=(unique_ptr<T>&& ptr_)
+	unique_ptr(unique_ptr<T>&& obj)noexcept : ptr(obj.ptr)
 	{
+		obj.ptr = nullptr;
+	}
+
+	unique_ptr& operator=(unique_ptr<T>&& obj) noexcept
+	{
+		if (this == &obj)
+			return *this;
 		delete ptr;
-		ptr = ptr_;
-		ptr_ = nullptr;
-	};
-
-	T operator*() const
-	{
-		return *ptr;
+		ptr = obj.ptr;
+		obj.ptr = nullptr;
+		return *this;
 	}
 
-	T* operator->() const
+
+
+
+
+	~unique_ptr()
 	{
-		return ptr;
+		deleter(ptr);
+	}
+};
+
+
+
+
+
+
+
+
+
+template<typename T, typename D >
+class unique_ptr<T[], D>
+{
+private:
+	T* ptr;
+	D deleter;
+public:
+	unique_ptr(T* ptr_ = nullptr)noexcept :ptr(ptr_) {};
+	unique_ptr(T* ptr_, const D& deleter_)noexcept :ptr(ptr_), deleter(deleter_) {};
+	unique_ptr(const unique_ptr<T>& obj) = delete;
+	unique_ptr& operator=(const unique_ptr<T>& obj) = delete;
+
+	unique_ptr(unique_ptr<T>&& obj)noexcept : ptr(obj.ptr)
+	{
+		obj.ptr = nullptr;
 	}
 
-	~unique_ptr() { delete ptr; }
+	unique_ptr& operator=(unique_ptr<T>&& obj) noexcept
+	{
+		if (this == &obj)
+			return *this;
+		delete ptr;
+		ptr = obj.ptr;
+		obj.ptr = nullptr;
+		return *this;
+	}
+
+	~unique_ptr()
+	{
+		deleter(ptr);
+	}
 };
